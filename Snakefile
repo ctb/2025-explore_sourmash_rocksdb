8998b938426ad99eda75b146622283981e3fb376
C. Titus Brown <titus@idyll.org>
2025-02-03 15:31:14 -0800

    upd

---
+++ Snakefile
@@ -0,0 +1,22 @@
+rule default:
+    input: "hashes.sig.zip",
+
+rule make_test_rocksdb:
+    input: "sig-mf.csv",
+    output: directory("tst.rocksdb"),
+    shell: "sourmash scripts index sig-mf.csv -o {output}"
+
+rule make_manifest_csv:
+    input: expand("sketches/{F}.fa.sig", F=[1, 2, 47, 63]),
+    output: "sig-mf.csv",
+    shell: "sourmash sig collect -F csv -o {output} {input}"
+    
+rule extract_rocksdb:
+    input: "tst.rocksdb"
+    output: "hashes.txt"
+    shell: "cargo run {input} -o {output}"
+
+rule make_sig:
+    input: "hashes.txt",
+    output: "hashes.sig.zip",
+    shell: "./hashes-into-sig.py {input} -o {output}"
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
+use std::fs::File;
+use std::io::Write;
 use byteorder::ReadBytesExt;
 use byteorder::LittleEndian;
+use clap::Parser;
+use std::path::PathBuf;
 
-use rocksdb::{ColumnFamilyDescriptor, Options};
-use rocksdb::{DB, Direction, IteratorMode};
-use rocksdb::MergeOperands;
+use rocksdb::IteratorMode;
 
-use sourmash::encodings::{Color, Colors, Idx};
 use sourmash::index::revindex::RevIndex;
 // use sourmash::index::revindex::disk_revindex::{ RevIndex };
 
 pub const HASHES: &str = "hashes";
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+struct Args {
+    /// Name of the person to greet
+    dbpath: PathBuf,
+
+    /// Number of times to greet
+    #[arg(short, long)]
+    outpath: PathBuf,
+}
+
 fn main() {
-    let revindex = RevIndex::open("tst.rocksdb", true, None).ok().expect("foo");
+    let cli = Args::parse();
+
+    let revindex = RevIndex::open(cli.dbpath, true, None).ok().expect("foo");
 
     let revindex = match revindex {
         RevIndex::Plain(db) => db,
@@ -21,12 +35,15 @@ fn main() {
     let db = revindex.db;
     let cf_handle = db.cf_handle(HASHES).expect("foo");
 
-    let mut iter = db.iterator_cf(&cf_handle, IteratorMode::Start); // Always iterates forward
+    let mut data_file = File::create(cli.outpath).expect("creation failed");
+
+    let iter = db.iterator_cf(&cf_handle, IteratorMode::Start); // Always iterates forward
     for item in iter {
         let (key, value) = item.unwrap();
 
         let k = (&key[..]).read_u64::<LittleEndian>().unwrap();
         //println!("Saw k={:?} k2={:?} v={:?}", key, k, value);
-        println!("{}", k);
+        writeln!(data_file, "{}", k).ok();
+        println!("{:?}", value);
     }
 }
